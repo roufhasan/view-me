63b623e2b3fcda6d91fab5e6973043f4843017c0
roufhasane <roufhasan5@gmail.comm>
2023-09-24 12:45:06 +0600

    responsive navbar added

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -18,14 +18,14 @@ const UserDetails = () => {
   console.log(user);
   return (
     <div className="max-w-screen-2xl mx-auto px-[3%] mt-10 dark:bg-slate-800">
-      <div className="flex gap-12">
+      <div className="flex flex-col md:flex-row gap-12">
         <div>
           <img
             src={user?.profile_photo}
             alt=""
             className="w-96 h-96 object-cover"
           />
-          <div className="flex gap-6 mt-12">
+          <div className="flex flex-wrap sm:flex-nowrap gap-6 mt-12">
             <button className="flex items-center gap-2 text-lg border-2 py-2 px-8 rounded-2xl border-purple-900 hover:bg-purple-900 hover:text-white dark:text-white transition-all">
               <FaPlus></FaPlus>
               Follow
@@ -37,14 +37,20 @@ const UserDetails = () => {
           </div>
         </div>
         <div>
-          <h2 className="text-6xl font-semibold text-gray-400">Hello,</h2>
-          <h2 className="text-6xl font-semibold mt-4 mb-8 dark:text-white">
+          <h2 className="text-4xl md:text-6xl font-semibold text-gray-400">
+            Hello,
+          </h2>
+          <h2 className="text-4xl md:text-6xl font-semibold mt-4 mb-8 dark:text-white">
             <span className="text-4xl font-normal text-gray-400">I&apos;m</span>{" "}
             {user?.name}
           </h2>
-          <p className="text-3xl font-bold dark:text-white">About Me</p>
+          <p className="text-2xl md:text-3xl font-bold dark:text-white">
+            About Me
+          </p>
           <hr className="horizontal border mt-2" />
-          <p className="mt-4 text-lg text-gray-400">{user?.details}</p>
+          <p className="mt-4 pb-6 md:pb-0 text-lg text-gray-400">
+            {user?.details}
+          </p>
         </div>
       </div>
     </div>
